3ac9c8fafe348ed94b53fc2cb8d7a1760045bc74
Marcus Sinkinson <sinkinsonmarcus@gmail.com>
2022-07-29 15:17:45 +0200

    git repo initialisation

---
+++ README.md
@@ -0,0 +1,21 @@
+## How to use
+
+This program uses Chrome as the default browser and will not work with other browsers.
+
+1. Install Chrome browser driver
+
+* Check your version of Chrome and use the below link to install the correct browser driver.
+* [Drivers](https://www.selenium.dev/documentation/webdriver/getting_started/install_drivers/)
+
+2. Add the driver to your path
+
+* Guide in link above
+* You can equally move the driver to an existing directory already in your path once downloaded.
+  For example if you have `usr/local/bin` in your PATH variable you can use the following:
+  `$ mv ~/path/to/chromedriver /usr/local/bin`
+* run the command `chromedriver` from the command line to ensure it starts up successfully, close once confirmed.
+
+3. Clone git repo
+4. `cd` into repo and run `npm install`
+5. Add `contacts.js` file to root of project
+6. Run `node adder.js` from the command line to execute the program
---
+++ adder.js
@@ -0,0 +1,57 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const {Builder, By} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const rlSync = require('readline-sync');
+
+import { contacts, baseUrl } from './contacts.js';
+
+let username;
+let password;
+let driver;
+
+function getCredentials() {
+  console.log('Please enter github credentials');
+  username = rlSync.question('username: ');
+  password = rlSync.question('password: ');
+}
+
+async function startBrowser() {
+  let options = new chrome.Options();
+  driver = await new Builder().setChromeOptions(options).forBrowser('chrome').build();
+}
+
+async function login() {
+  await driver.get('https://www.github.com/login');
+  await driver.findElement(By.id('login_field')).sendKeys(username);
+  await driver.findElement(By.id('password')).sendKeys(password);
+  await driver.findElement(By.name('commit')).click();
+}
+
+async function addContacts(contacts) {
+  try {
+    for (const contact of contacts) {
+      await driver.get(`${baseUrl}${contact}`);
+      const selector = "input[value='Follow'][class='btn btn-block']";
+      let element = await driver.findElement(By.css(selector));
+      if (await element.isDisplayed() && await element.isEnabled()) {
+        await element.click();
+        console.log(`Added ${contact}`);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  
+  await driver.quit();
+}
+
+async function main() {
+  getCredentials();
+  await startBrowser();
+  await login();
+  addContacts(contacts);
+}
+
+main();
